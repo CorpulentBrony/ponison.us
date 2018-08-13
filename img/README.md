8604b9bdccac7fdf47f96c06307dff9323c24a30
Corpulent Brony <corpulent@brony.email>
2018-08-13 18:01:51 +0000

    Playing with bg image and metadata * Added a bg image to the site that sits in the bottom right unobtrusively * Completing metadata, including the addition of a manifest and sitemap * JS change to load bg image only after stylesheets have finished loading

--- img/README.md
+++ img/README.md
@@ -3,3 +3,4 @@ This directory contains images used in the application.
 
 * `twiggle_face_square.png`: Art by LockHeart, cropped version of http://9volt-nsfw.tumblr.com/post/176674371688/commission-for-corpulent-thanks-so-much-for
 * `twiggle_full.png`: Art by LockHeart, full version of http://9volt-nsfw.tumblr.com/post/176674371688/commission-for-corpulent-thanks-so-much-for
+* `twiggle_full/`: Art by LockHeart, contains versions of `twiggle_full.png` in various sizes and formats
--- module/index.mjs
+++ module/index.mjs
@@ -1,9 +1,15 @@
 "use strict";
+import { elements } from "./Elements.mjs";
 import { Form } from "./Form.mjs";
 import { isDocumentLoaded, loadDeferredStylesheets } from "./util.mjs";
 
 (async function onDocumentLoad() {
-	loadDeferredStylesheets().catch(console.error);
+	async function loadDeferredStylesheetsAndBg() {
+		await loadDeferredStylesheets();
+		window.document.body.appendChild(elements.templateBackgroundImage.content);
+	}
+
+	loadDeferredStylesheetsAndBg().catch(console.error);
 	await isDocumentLoaded;
 	const form = new Form();
 	form.init();
